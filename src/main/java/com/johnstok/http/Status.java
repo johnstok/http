c2b52e93629c11621d3db3b7004c9b226faadcde
Keith Webster Johnston <k.w.johnston@gmail.com>
2013-03-10 13:51:41 +0000

    Documented informational status codes.

--- src/main/java/com/johnstok/http/Status.java
+++ src/main/java/com/johnstok/http/Status.java
@@ -25,8 +25,47 @@ package com.johnstok.http;
 })
 public enum Status {
 
-    CONTINUE(                       100, "Continue"),
-    SWITCHING_PROTOCOLS(            101, "Switching Protocols"),
+    /** CONTINUE : Status.
+     *
+     * <pre>
+   The client SHOULD continue with its request. This interim response is
+   used to inform the client that the initial part of the request has
+   been received and has not yet been rejected by the server. The client
+   SHOULD continue by sending the remainder of the request or, if the
+   request has already been completed, ignore this response. The server
+   MUST send a final response after the request has been completed. See
+   section 8.2.3 for detailed discussion of the use and handling of this
+   status code.
+     * </pre>
+     */
+    @Specifications({
+        @Specification(name="rfc-2161", section="8.2.3"),
+        @Specification(name="rfc-2161", section="10.1.1")
+    })
+    CONTINUE(                       100, "Continue"),              //$NON-NLS-1$
+
+    /** SWITCHING_PROTOCOLS : Status.
+     *
+     * <pre>
+   The server understands and is willing to comply with the client's
+   request, via the Upgrade message header field (section 14.42), for a
+   change in the application protocol being used on this connection. The
+   server will switch protocols to those defined by the response's
+   Upgrade header field immediately after the empty line which
+   terminates the 101 response.
+
+   The protocol SHOULD be switched only when it is advantageous to do
+   so. For example, switching to a newer version of HTTP is advantageous
+   over older versions, and switching to a real-time, synchronous
+   protocol might be advantageous when delivering resources that use
+   such features.
+     * </pre>
+     */
+    @Specifications({
+        @Specification(name="rfc-2161", section="14.42"),
+        @Specification(name="rfc-2161", section="10.1.2")
+    })
+    SWITCHING_PROTOCOLS(            101, "Switching Protocols"),   //$NON-NLS-1$
 
     OK(                             200, "OK"),
     CREATED(                        201, "Created"),
@@ -150,8 +189,30 @@ public enum Status {
     /**
      * Check if the status is informational.
      *
+     * <pre>
+   This class of status code indicates a provisional response,
+   consisting only of the Status-Line and optional headers, and is
+   terminated by an empty line. There are no required headers for this
+   class of status code. Since HTTP/1.0 did not define any 1xx status
+   codes, servers MUST NOT send a 1xx response to an HTTP/1.0 client
+   except under experimental conditions.
+
+   A client MUST be prepared to accept one or more 1xx status responses
+   prior to a regular response, even if the client does not expect a 100
+   (Continue) status message. Unexpected 1xx status responses MAY be
+   ignored by a user agent.
+
+   Proxies MUST forward 1xx responses, unless the connection between the
+   proxy and its client has been closed, or unless the proxy itself
+   requested the generation of the 1xx response. (For example, if a
+   proxy adds a "Expect: 100-continue" field when it forwards a request,
+   then it need not forward the corresponding 100 (Continue)
+   response(s).)
+     * </pre>
+     *
      * @return True if the request is informational; false otherwise.
      */
+    @Specification(name="rfc-2616", section="10.1")
     public boolean isInformational() {
         return 1==(_code/100);
     }
