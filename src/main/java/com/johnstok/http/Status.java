5cf392e3e50ab41e5e751e7f966f2ac64367fe63
Keith Webster Johnston <k.w.johnston@gmail.com>
2013-03-10 14:07:25 +0000

    Documented redirection status codes.

--- src/main/java/com/johnstok/http/Status.java
+++ src/main/java/com/johnstok/http/Status.java
@@ -231,8 +231,23 @@ public enum Status {
     /**
      * Check if the status is a redirect.
      *
+     * <pre>
+   This class of status code indicates that further action needs to be
+   taken by the user agent in order to fulfill the request.  The action
+   required MAY be carried out by the user agent without interaction
+   with the user if and only if the method used in the second request is
+   GET or HEAD. A client SHOULD detect infinite redirection loops, since
+   such loops generate network traffic for each redirection.
+
+      Note: previous versions of this specification recommended a
+      maximum of five redirections. Content developers should be aware
+      that there might be clients that implement such a fixed
+      limitation.
+     * </pre>
+     *
      * @return True if the request is a redirect; false otherwise.
      */
+    @Specification(name="rfc-2616", section="10.3")
     public boolean isRedirect() {
         return 3==(_code/100);
     }
