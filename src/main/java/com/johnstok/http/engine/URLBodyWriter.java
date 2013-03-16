18b6ef907c0abde3657ea8ba51ee9b67c4833130
Keith Webster Johnston <k.w.johnston@gmail.com>
2013-03-16 16:25:00 +0000

    Added a body writer that reads from an URL.

---
+++ src/main/java/com/johnstok/http/engine/URLBodyWriter.java
@@ -0,0 +1,58 @@
+/*-----------------------------------------------------------------------------
+ * Copyright © 2013 Keith Webster Johnston.
+ * All rights reserved.
+ *
+ * This file is part of http.
+ *
+ * http is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * http is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with http. If not, see <http://www.gnu.org/licenses/>.
+ *---------------------------------------------------------------------------*/
+package com.johnstok.http.engine;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+
+/**
+ * A body writer that reads from a URL.
+ *
+ * @author Keith Webster Johnston.
+ */
+public final class URLBodyWriter
+    implements
+        BodyWriter {
+
+    private final URL _url;
+
+
+    /**
+     * Constructor.
+     *
+     * @param url The URL to read from.
+     */
+    public URLBodyWriter(final URL url) {
+        _url = Utils.checkNotNull(url);
+    }
+
+
+    @Override
+    public void write(final OutputStream os) throws IOException {
+        final InputStream is = _url.openStream();
+        try {
+            Utils.copy(is, os);
+        } finally {
+            Utils.close(is);
+        }
+    }
+}
